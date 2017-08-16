058269c88f8d5f0676df1818192ffbf853c31a90
mabcbot <ing.manuelbustamante@gmail.com>
2017-08-16 17:07:26 -0500

    versión final de vectores.py
    Ya se han agregado todas las funciones necesarias, además de el manejo de excepciones

--- vectores.py
+++ vectores.py
@@ -20,23 +20,30 @@ def cls():					# Funcion para limpiar pantalla en cualquier S.O.
 
 def creaVector():
 	#dim = int(input("Ingresa de que dimension sera el vector -->\t"))
-	v = input("\nIngrese las componentes del vector separadas por un espacio\n-->\t")
-	v = v.split(" ")
-	vector = list(map(lambda n: float(n), v))
-	
-	#print(vector)
-	#print(type(vector))
-	return vector
+	while True:
+		v = input("\nIngrese las componentes del vector separadas por un espacio\n-->\t")
+		v = v.split(" ")
+		try:
+			vector = list(map(lambda n: float(n), v))
+			#print(vector)
+			#print(type(vector))
+			return vector
+		except ValueError:
+			print("\n\t*Error* Ingrese solo valores numericos")
 
 def suma():
 	print("\n========= SUMA =========\n")
 	num = int(input("Ingrese cuantos vectores desea sumar -->\t"))
 	vectores = []
-	total = []
+	#total = []
 	for i in range(0,num):
 		vectores.append(creaVector())
 	print(vectores)
 	total = vectores[0]
+	temp = list(filter(lambda n: len(n) == len(total), vectores))
+	if len(temp) != len(vectores):
+		print("\n\t*Error* Ambos vectores deben tener la misma dimension")
+		return 0
 	jump = True
 	for i in vectores:
 		if not jump:
@@ -56,6 +63,10 @@ def resta():
 		vectores.append(creaVector())
 	print(vectores)
 	total = vectores[0]
+	temp = list(filter(lambda n: len(n) == len(total), vectores))
+	if len(temp) != len(vectores):
+		print("\n\t*Error* Ambos vectores deben tener la misma dimension")
+		return 0
 	jump = True
 	for i in vectores:
 		if not jump:
@@ -69,41 +80,78 @@ def resta():
 def producto():
 	print("\n========= PRODUCTO =========")
 	vector = creaVector()
-	escalar = float(input("Ingrese el escalar que multiplicara al vector -->\t"))
-	print("\n\t El producto es: " + str(list(map(lambda n: escalar * n, vector))))
+	while True:
+		try:
+			escalar = float(input("Ingrese el escalar que multiplicara al vector -->\t"))
+			print("\n\t El producto es: " + str(list(map(lambda n: escalar * n, vector))))
+			return 0
+		except ValueError:
+			print("\n\t*Error* Ingrese solo valores numericos")
 
-def norma():
-	print("\n========= NORMA =========")
-	vector = creaVector()
-	temp = list(map(lambda n: n ** 2, vector))
-	temp1 = 0
-	for i in temp:
-		temp1 += i
-	print("\n\t La norma es: " + math.sqrt(temp1))
+def norma(vect):
+	try:
+		#vector = creaVector()
+		temp = list(map(lambda n: n ** 2, vect))
+		temp1 = 0
+		for i in temp:
+			temp1 += i
+		modulo = math.sqrt(temp1)
+		return modulo
+	except TypeError:
+		pass
+
+def angulo():
+	# Angulo entre vectores = coseno inverso de ((u * v)/(||u||*||v||))
+	print("\n========= ANGULO ENTRE VECTORES =========")
+	v1 = creaVector()
+	v2 = creaVector()
+	if len(v1) != len(v2):
+		print("\n\t*Error* Ambos vectores deben tener la misma dimension")
+		return 0
+	mod1 = norma(v1)
+	mod2 = norma(v2)
+	j = 0
+	temp = 0.0
+	temp1 = 0.0
+	for i in v1:
+		temp += (i * v2[j])
+		j += 1
+	temp1 = round(mod1 * mod2, 2)
+	#print(temp)
+	#print(temp1)
+	temp = temp / temp1
+	print("El angulo entre los vectores es " + str(math.degrees(math.acos(temp))) + " grados")
 
 while True:
-	print("\n========= Programa encargado de hacer operaciones con vectores =========\n"
-              "    \t1. Suma\n"
-              "    \t2. Resta\n"
-              "    \t3. Producto por escalar\n"
-              "    \t4. Norma\n"
-              "    \t5. Angulo entre dos vectores\n"
-              "    \t0. Salir")
-	opcion = int(input("-->\t"))
-	if opcion:
-		cls()
-	if opcion == 1:		# Suma
-		suma()
-	elif opcion == 2:	# Resta
-		resta()
-	elif opcion == 3:	# Producto
-		producto()
-	elif opcion == 4:	# Norma
-		norma()
-	elif opcion == 5:	# Angulo
-		pass
-	elif opcion == 0:
-		break
-	else:
-		print("\n\t\tOpcion invalida, intente de nuevo")
-#creaVector()
+	try:
+		print("\n========= Programa encargado de hacer operaciones con vectores n-dimensionales =========\n"
+	              "    \t1. Suma\n"
+	              "    \t2. Resta\n"
+	              "    \t3. Producto por escalar\n"
+	              "    \t4. Norma\n"
+	              "    \t5. Angulo entre dos vectores\n"
+	              "    \t0. Salir")
+		opcion = int(input("-->\t"))
+		if opcion:
+			cls()
+		if opcion == 1:		# Suma
+			suma()
+		elif opcion == 2:	# Resta
+			resta()
+		elif opcion == 3:	# Producto
+			producto()
+		elif opcion == 4:	# Norma
+			print("\n========= NORMA =========")
+			vect = creaVector()
+			mod = norma(vect)
+			if mod:
+				print("\n\t La norma es: " + str(mod))
+		elif opcion == 5:	# Angulo
+			angulo()
+		elif opcion == 0:
+			break
+		else:
+			print("\n\t\tOpcion invalida, intente de nuevo")
+	#creaVector()
+	except ValueError:
+		print("\n\tValor erroneo")
