be2f6edd2dbbed800b2839dafd1d56c7c2f46ca8
mabcbot <ing.manuelbustamante@gmail.com>
2017-09-01 21:20:37 -0500

    versión final y comentada

--- cifradoAlternativo.py
+++ cifradoAlternativo.py
@@ -36,12 +36,16 @@ def decript(encriptedMessage):
 
 while True:
 	try:
+		# Se define el diccionario que se usará para codificar los mensajes además del espacio para permitir
+		# comprender mejor los mensajes
 		encriptionKey = {"a" : "AA", "b" : "AB", "c" : "AC", "d" : "AD", "e" : "AE",
 						 "f" : "BA", "g" : "BB", "h" : "BC", "i" : "BD", "j" : "BD", "k" : "BE",
 						 "l" : "CA", "m" : "CB", "n" : "CC", "o" : "CD", "p" : "CE",
 						 "q" : "DA", "r" : "DB", "s" : "DC", "t" : "DD", "u" : "DE",
 						 "v" : "EA", "w" : "EB", "x" : "EC", "y" : "ED", "z" : "EE", " ": " "}
 
+		# Se define el diccionario que se usará para decodificar los mensajes además del espacio para permitir
+		# comprender mejor los mensajes
 		decriptionKey = {"AA" : "a", "AB" : "b", "AC" : "c", "AD" : "d", "AE" : "e",
 						 "BA" : "f", "BB" : "g", "BC" : "h", "BD" : "i", "BD" : "j", "BE" : "k",
 						 "CA" : "l", "CB" : "m", "CC" : "n", "CD" : "o", "CE" : "p",
@@ -63,14 +67,21 @@ while True:
 			if choice == 1:
 				text = str(input("Ingrese el texto a codificar:\n->\t"))
 				text = text.lower()
-				print("\tMensaje cifrado:\n" + encript(text))
+				encoded = encript(text)
+				print("\tMensaje cifrado:\n" + encoded)
+				result = open("codificado.txt", "w")
+				result.write(encoded)				# Se guarda el mensaje cifrado en un archivo que se crea exclusivamente para ello
+				result.close()
+				print("\n====================== El mensaje codificado se guardo en el archivo codificado.txt ======================")
 			elif choice == 2:
 				name = str(input("Ingrese el nombre del archvivo con su extension\n->\t"))
 				text = open(name, "r")
 				result = open("codificado.txt", "w")
 				for line in text:
+					# Con esto evitamos que la función de encriptación intente codificar el salto de línea o el símbolo de fin de archivo
 					temp = line[0 : (len(line) - 1)]
-					result.write(encript(temp) + "\n") # Esta linea solo se ocupa en caso de leer el texto de un archivo
+					# Se concatena cada renglón del texto mediante un salto de línea para permitir conservar la estructura original del escrito
+					result.write(encript(temp) + "\n")
 				text.close()
 				result.close()
 				print("============================== Proceso finalizado ==============================\n")
@@ -78,7 +89,7 @@ while True:
 				for line in result:
 					print(line)
 				result.close()
-				print("\n============================== El mensaje codificado se guardo en el archivo codificado.txt ==============================")
+				print("\n====================== El mensaje codificado se guardo en el archivo codificado.txt ======================")
 		elif option == 2:
 			cls()
 			print("\n============================== Descifrado ==============================\n"
@@ -88,22 +99,29 @@ while True:
 			if choice == 1:
 				text = str(input("Ingrese el texto a decodificar:\n->\t"))
 				text = text.upper()
-				print("\tMensaje cifrado:\n" + decript(text))
+				decoded = decript(text)
+				print("\tMensaje cifrado:\n" + decoded)
+				result = open("decodificado.txt", "w")
+				result.write(decoded)	# Se guarda el mensaje una vez descifrado en el archivo creado para ello
+				result.close()
+				print("\n====================== El mensaje decodificado se guardo en el archivo decodificado.txt ======================")
 			elif choice == 2:	
 				name = str(input("Ingrese el nombre del archvivo con su extension\n->\t"))
 				text = open(name, "r")
 				result = open("decodificado.txt", "w")
 				for line in text:
+					# Con esto evitamos que la función de encriptación intente codificar el salto de línea o el símbolo de fin de archivo
 					temp = line[0 : (len(line) - 1)]
-					result.write(decript(temp) + "\n")
+					# Se concatena cada renglón del texto mediante un salto de línea para permitir conservar la estructura original del escrito
+					result.write(decript(temp) + "\n")	# Se guarda el mensaje una vez descifrado en el archivo creado para ello
 				text.close()
 				result.close()
 				print("============================== Proceso finalizado ==============================\n")
-				result = open("test.txt", "r")
+				result = open("decodificado.txt", "r")
 				for line in result:
 					print(line)
 				result.close()
-				print("\n============================== El mensaje decodificado se guardo en el archivo decodificado.txt ==============================")
+				print("\n====================== El mensaje decodificado se guardo en el archivo decodificado.txt ======================")
 		elif option == 0:
 			print("""\t\t\t\t\t____________________________________
 					Developed by 
