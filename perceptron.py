12498220be70bc92ab9977eab36bd9eab0bcb455
mabcbot <ing.manuelbustamante@gmail.com>
2017-12-02 23:53:32 -0600

    Update perceptron.py
    Ya funciona con más ejemplos, solo falta pulir unos detalles para que se vea mejor

--- perceptron.py
+++ perceptron.py
@@ -9,33 +9,44 @@
 # Juárez Pascual Karla Vanessa
 # Hecho en Python 3.5
 
-from functools import reduce
+#from functools import reduce
+import os
 
 
-def perceptron(muestras, resultados, pesos, taza=0.1):
+def perceptron(muestras, resultados, pesos, taza = 0.1):
     old_pesos = []
 
     while old_pesos != pesos:
-        resultados_iter = iter(resultados)
+        #resultados_iter = iter(resultados)
+        i = 0
+        old_pesos = pesos[:]
         for muestra in muestras:
-            salida = reduce((lambda x, y: x+y), [p * m for p, m in zip(pesos, muestra)])
-            resultado = get_resultado_actual(resultados_iter)
-            delta = taza * (resultado - g(salida))
-            old_pesos = pesos[:]
-            if delta != 0:
-                pesos = actualiza_pesos(pesos, muestra, delta)
-        print(pesos)
+            salida = 0
+            temp = list(map(lambda x, y: x * y, pesos, muestra))
+            #print(temp)
+            for n in temp:
+                salida += n
+            #resultado = get_resultado_actual(resultados_iter)
+            delta = taza * (resultados[i] - g(salida))
+            #print("\n\t", delta)
+            #if delta != 0:
+            pesos = actualiza_pesos(pesos, muestra, delta)
+                #print(pesos)
+            i += 1
+        print("\t\t", pesos)
+        #os.system("pause")
     return pesos
 
-
+"""
 def get_resultado_actual(r):
     return next(r)
-
+"""
 
 def actualiza_pesos(pesos, muestra, delta):
+    #print("Actualizando pesos")
     n_pesos = []
     for m, p in zip(muestra, pesos):
-        p = p + m*delta
+        p = p + m * delta
         n_pesos.append(p)
     return n_pesos
 
@@ -53,7 +64,7 @@ def main():
                 [1, 0, 1],
                 [1, 1, 0],
                 [1, 1, 1]]
-    resultados = [1, 1, 1, -1]
+    resultados = [-1, 1, 1, -1]
     taza = 0.1
     pesos = [0, 0, 0]
 
