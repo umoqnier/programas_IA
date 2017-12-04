3f539c30ccb4a30d53076b57210ce3bc76128c68
Diego Alberto Barriga Martínez <umoqnier@gmail.com>
2017-12-04 00:40:36 -0600

    K medias terminado y correccion en perceptron para no actualizar pesos si delta es igual a cero. Se agregan muestras de la presentación al perceptron para pruebas.

--- k_medias.py
+++ k_medias.py
@@ -14,16 +14,27 @@ from math import sqrt
 
 def k_medias(k):
     """ Clasificador por K-medias """
-    bandera = k - 1
-    muestras = [[8, 10], [3, 10.5], [7, 13.5], [5, 18], [5, 13],
-                [6, 9], [9, 11], [3, 18], [8.5, 12], [8, 16]]
-    aux_muestras = muestras[:]
-    medias_iniciales = [muestras[i] for i in sample(range(0, len(muestras)), k)]
-    nuevas_medias = []
-    pre_medias = []
-    while medias_iniciales != nuevas_medias:
+    # ========General========
+    # medias_iniciales = [muestras[i] for i in sample(range(0, len(muestras)), k)] ALEATORIAS
+
+    # ========Ejercicio en clase========
+    # muestras = [[8, 10], [3, 10.5], [7, 13.5], [5, 18], [5, 13],
+    #            [6, 9], [9, 11], [3, 18], [8.5, 12], [8, 16]]
+    # medias_iniciales = [[8, 10], [5, 13]]
+
+    # ========Tarea========
+    muestras = [[1, 12.5], [3, 10.5], [3, 12.5], [3, 14.5], [3, 18],
+                [5, 18], [5, 16], [5, 14.5], [5, 13], [6, 9], [8, 10],
+                [9, 11], [8.5, 12], [7, 13.5], [8, 16], [0.5, 10.5]]
+    medias_iniciales = [[3, 14.5], [9, 11]]
+    while True:
         conjunto_distancias = obtener_distancias(medias_iniciales, muestras)  # Lista de diccionarios {d: punto}
-        c = formar_conjuntos(conjunto_distancias)
+        puntos_clasificados = formar_conjuntos(conjunto_distancias, k)
+        nuevas_medias = actualiza_medias(puntos_clasificados)
+        if nuevas_medias != medias_iniciales:
+            medias_iniciales = nuevas_medias[:]
+        elif nuevas_medias == medias_iniciales:
+            return medias_iniciales, puntos_clasificados
 
 
 def obtener_distancias(medias, muestras):
@@ -33,32 +44,69 @@ def obtener_distancias(medias, muestras):
     for media in medias:
         for muestra in muestras:
             for i in range(len(media) - 1):  # Para vectores de n dimensiones
-                d = sqrt(((muestra[i+1] - media[i+1])**2) + ((muestra[i] - media[i])**2))
+                d = (sqrt(((muestra[i+1] - media[i+1])**2) + ((muestra[i] - media[i])**2)))
                 distancias.append({d: muestra})
         conjunto_distancias.append(distancias)
         distancias = []
     return conjunto_distancias
 
 
-def formar_conjuntos(conjunto_d):
+def formar_conjuntos(conjunto_d, k):
     """ Funcion que forma los conjuntos necesarios tomando la menor distancia """
-    clasificaciones = []  # TODO: Crear lista con listas vacias. Tantas como clasifiaciones tengamos
+    clasificaciones = crear_listas_vacias(k)
     elements = []
     tam_dist = len(conjunto_d[0])  # Tamaño del conjunto de distancias
     for j in range(tam_dist):
         for i in range(len(conjunto_d)):
-            elements.append(conjunto_d[i][j].keys())  # Se guarda la columna en una lista
+            elements.append(list(conjunto_d[i][j].keys()).pop())  # Se guarda la columna en una lista
         menor = min(elements)  # Obtiene el menor de la columna
         indice = elements.index(menor)  # Obtiene el indice para asignarlo a un conjunto
         elements = []
-        clasificaciones.append(indice)  # Interesa el indice para obtener los vectores de la clasificacion
+        clasificaciones[indice].append(conjunto_d[indice][j].get(menor))
 
     return clasificaciones
 
 
+def crear_listas_vacias(k):
+    espacio = []
+    while k != 0:
+        espacio.append(list())
+        k -= 1
+    return espacio
+
+
+def actualiza_medias(conjunto):
+    """ Funcion que actualiza las medias """
+    suma = 0
+    coordenada = 0
+    nuevas_medias = []
+    media = []
+    dimension = len(conjunto[0][0])  # Se asume que todos los puntos son de la misma dimension
+    for puntos in conjunto:
+        while dimension > coordenada:
+            for punto in puntos:
+                for i in range(coordenada, len(punto), 2):
+                    suma += punto[i]
+
+            prom = suma / len(puntos)
+            media.append(prom)
+            coordenada += 1
+            suma = 0
+        nuevas_medias.append(media)
+        media = []
+        coordenada = 0
+
+    return nuevas_medias
+
+
 def main():
     k = 2
-    k_medias(k)
+    i = 0
+    medias, conjuntos_clasificados = k_medias(k)
+    print("Medias -->", medias)
+    for puntos in conjuntos_clasificados:
+        print("Conjunto:", i+1, "-->", puntos)
+        i += 1
 
 
 if __name__ == '__main__':
--- perceptron.py
+++ perceptron.py
@@ -9,24 +9,27 @@
 # Juárez Pascual Karla Vanessa
 # Hecho en Python 3.5
 
-def perceptron(muestras, resultados, pesos, taza = 0.1):
-    pesosPrev = []
-    while pesosPrev != pesos:
+
+def perceptron(muestras, resultados, pesos, taza=0.1):
+    pesos_prev = []
+    while pesos_prev != pesos:  # Condicion de salida del algoritmo
         i = 0
-        pesosPrev = pesos[:]
+        pesos_prev = pesos[:]
         for muestra in muestras:
             salida = 0
             temp = list(map(lambda x, y: x * y, pesos, muestra))
             for n in temp:
                 salida += n
             delta = taza * (resultados[i] - g(salida))
-            pesos = updatePesos(pesos, muestra, delta)
+            if delta != 0:
+                pesos = update_pesos(pesos, muestra, delta)
             i += 1
         print("\t\tPesos->\t", pesos)
     return pesos
 
 
-def updatePesos(pesos, muestra, delta):
+def update_pesos(pesos, muestra, delta):
+    """ Funcion que actualiza pesos """
     n_pesos = []
     for m, p in zip(muestra, pesos):
         p = p + m * delta
@@ -35,25 +38,30 @@ def updatePesos(pesos, muestra, delta):
 
 
 def g(pulse):
-    if pulse >= 0:      # Se define el umbral en 0
+    """ Funcion pulso unitario """
+    if pulse >= 0:  # Se define el umbral en 0
         return 1
     else:
         return -1
 
 
 def main():
-             # x0, x1, x2
+              # x0, x1, x2
     muestras = [[1, 0, 0],
                 [1, 0, 1],
                 [1, 1, 0],
                 [1, 1, 1]]
     resultados = [-1, 1, 1, -1]
+    # muestras = [[1, 0, 0],
+    #             [1, 0, 1],
+    #             [1, 1, 0],
+    #             [1, 1, 1]]
+    # resultados = [1, 1, 1, -1]
     taza = 0.1
-    # Pesos iniciales igual a 0
-    pesosInit = [0, 0, 0]
+    pesos_init = [0, 0, 0] # Pesos iniciales igual a 0
 
-    pesosResult = perceptron(muestras, resultados, pesosInit, taza)
-    print("\n\tPesos Finales:\t", pesosResult)
+    pesos_result = perceptron(muestras, resultados, pesos_init, taza)
+    print("\n\tPesos Finales:\t", pesos_result)
 
 
 if __name__ == '__main__':
